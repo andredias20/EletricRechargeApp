d7368eb74a30a2f76482830c20dff520a86282f4
andredias20 <andre.c.dias2000@gmail.com>
2024-03-06 02:05:47 -0300

    first commit

---
+++ app/src/main/java/com/github/andredias20/eletriccarapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.github.andredias20.eletriccarapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.github.andredias20.eletriccarapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
